18efa6c775d4f08a210c1269d723054c03e161a9
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-05-24 22:15:07 +0200

    Update build.c

--- build.c
+++ build.c
@@ -1,46 +1,57 @@
 #include "main.h"
 
 /**
- * my_exit - simple impl of exit.
- * @ptrs: structure containing all malloced memory
+ * my_exit - Exit the shell with a specified error code
+ * @pts: Pointer to the shell_t structure
  */
 void my_exit(shell_t *pts)
 {
-    unsigned int x;
-    char *exit_str;
-    exit_str = pts->input_token[1];
-    if(exit_str != NULL || pts == NULL)
-    {
-        errno = 0;
-        for(x = 0; exit_str[x] != '\0'; x++)
-            errno = errno * 10 + (exit_str[x] - '0');
-    }
-    free_shell_t(pts);
-    if(errno > 255)
-        errno %= 256;
-    exit(errno);
+	unsigned int x;
+    
+	char *exit_str;
+    
+	exit_str = pts->input_token[1];
+    
+	if (exit_str != NULL || pts == NULL)
+	{
+		errno = 0;
+		for (x = 0; exit_str[x] != '\0'; x++)
+			errno = errno * 10 + (exit_str[x] - '0');
+        
+	}
+	free_shell_t(pts);
+    
+	if (errno > 255)
+		errno %= 256;
+	exit(errno);
 }
+
 /**
-  * print_env - prints out the current environment
-  * @ptrs: structure containing all malloced memory
-  */
+ * print_env - Print the environment variables
+ * @pts: Pointer to the shell_t structure
+ */
+
 void print_env(shell_t *pts)
 {
-    unsigned int x, y;
+	unsigned int x, y;
     
-    char enld = '\n';
-    (void)pts;
+	char enld = '\n';
+
+	(void)pts;
+
+	if (environ == NULL)
+		return;
     
-    if(environ == NULL)
-        return;
-    for(x = 0; environ[x] != NULL; x++)
-    {
-        y = _strlen(environ[x]);
-        if(y != 0)
-        {
-            write(STDOUT_FILENO, environ[x], y);
-            write(STDOUT_FILENO, &enld, 1);
-        }
-    }
-    errno = 0;
+	for (x = 0; environ[x] != NULL; x++)
+	{
+		y = _strlen(environ[x]);
+        
+		if (y != 0)
+		{
+			write(STDOUT_FILENO, environ[x], y);
+            
+			write(STDOUT_FILENO, &enld, 1);
+		}
+	}
+	errno = 0;
 }
