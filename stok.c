6cd3ed0a35cee18ce4c99e3f6dd8b7d29cf1d821
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-05-24 21:08:22 +0200

    Update stok.c

--- stok.c
+++ stok.c
@@ -1,92 +1,51 @@
 #include "main.h"
 
-
 /**
-
-  * _strtok - tokenizes a string
-
-  * @str: string to tokenize
-
-  * @delim: string that contains the delimiters
-
-  * Return: pointer to position in string of a null terminated word.
-
-  */
-
+ * _strtok - tokenizes a string
+ * @str: string to tokenize
+ * @delimiter: string that contains the delimiters
+ * Return: pointer to position in string of a null-terminated word.
+ */
 char *_strtok(char *str, const char *delimiter)
-
 {
+    static char *w_start, *end_str, *end;
+    int word_found = 0;
 
-	static char *w_start, *end_str, *end;
+    if (str == NULL)
+    {
+        if (w_start == NULL || end == NULL)
+            return NULL;
+        str = end + 1;
+    }
+    else
+        end_str = str + strlen(str);
 
-	int word_found = 0;
+    for (int x = 0; str + x < end_str; x++)
+    {
+        int y;
+        for (y = 0; delimiter != NULL && delimiter[y] != '\0'; y++)
+        {
+            if (str[x] == delimiter[y])
+            {
+                if (word_found == 1)
+                {
+                    str[x] = '\0';
+                    end = str + x;
+                    return w_start;
+                }
+                break;
+            }
+        }
+        if (delimiter[y] == '\0' && word_found == 0)
+        {
+            word_found = 1;
+            w_start = str + x;
+        }
+    }
 
+    end = NULL;
+    if (word_found == 1)
+        return w_start;
 
-
-	if (str == NULL)
-
-	{
-
-		if (w_start == NULL || end == NULL)
-
-			return (NULL);
-
-		str = end + 1;
-
-	}
-
-	else
-
-		end_str = str + _strlen(str);
-
-	for (int x = 0; str + x < end_str; x++)
-
-	{
-
-		for (int y = 0; delimiter != NULL && delimiter[y] != '\0'; y++)
-
-		{
-
-			if (str[x] == delimiter[j])
-
-			{
-
-				if (word_found == 1)
-
-				{
-
-					str[x] = '\0';
-
-					end = str + x;
-
-					return (w_start);
-
-				}
-
-				break;
-
-			}
-
-		}
-
-		if (delimiter[y] == '\0' && word_found == 0)
-
-		{
-
-			word_found = 1;
-
-			w_start = str + x;
-
-		}
-
-	}
-
-	end = NULL;
-
-	if (word_found == 1)
-
-		return (w_start);
-
-	return (NULL);
-
+    return NULL;
 }
