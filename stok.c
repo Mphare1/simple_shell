91b29f98f3ca95d8dc64fb85050a9ce02311139d
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-05-24 21:40:06 +0200

    Update stok.c

--- stok.c
+++ stok.c
@@ -1,52 +1,53 @@
 #include "main.h"
+#include <stdbool.h>
 
 /**
- * _strtok - tokenizes a string
- * @str: string to tokenize
- * @delimit_: string that contains the delimiters
- * Return: pointer to position in string of a null-terminated word.
+ * _strtok - custom implementation of strtok function
+ * @str: the string to be tokenized
+ * @delimit_: the delimiter used to tokenize the string
+ *
+ * Return: a pointer to the next token found in the string, or NULL if no more tokens are found
  */
 char *_strtok(char *str, const char *delimit_)
 {
-    unsigned int i, j;
+    unsigned int x, y;
     static char *w_start, *endd_, *end2;
-    int word_found = 0;
-    
-    if(str == NULL)
+    bool word_found = false;
+
+    if (str == NULL)
     {
-        if(w_start == NULL || end2 == NULL)
+        if (w_start == NULL || end2 == NULL)
             return NULL;
         str = end2 + 1;
     }
     else
         endd_ = str + _strlen(str);
-        
-    for(i = 0; str + i < endd_; i++)
+
+    for (x = 0; str + x < endd_; x++)
     {
-      
-        for(j = 0; delimit_ != NULL && delimit_[j] != '\0'; j++)
+        for (y = 0; delimit_ != NULL && delimit_[y] != '\0'; y++)
         {
-            if(str[i] == delimit_[j])
+            if (str[x] == delimit_[y])
             {
-                if(word_found == 1)
+                if (word_found)
                 {
-                    str[i] = '\0';
-                    end2 = str + i;
+                    str[x] = '\0';
+                    end2 = str + x;
                     return w_start;
                 }
                 break;
             }
         }
-        if(delimit_[j] == '\0' && word_found == 0)
+        if (delimit_[y] == '\0' && !word_found)
         {
-            word_found = 1;
-            w_start = str + i;
+            word_found = true;
+            w_start = str + x;
         }
     }
-    
+
     end2 = NULL;
-    if(word_found == 1)
+    if (word_found)
         return w_start;
-        
+
     return NULL;
 }
