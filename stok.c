e608c3764d3c8f30cb6e4861409bdd611c998e7f
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-05-24 21:21:08 +0200

    Update stok.c

--- stok.c
+++ stok.c
@@ -3,49 +3,48 @@
 /**
  * _strtok - tokenizes a string
  * @str: string to tokenize
- * @delim: string that contains the delimiters
+ * @delimit_: string that contains the delimit_iters
  * Return: pointer to position in string of a null-terminated word.
  */
-char *_strtok(char *str, const char *delim)
+char *_strtok(char *str, const char *delimit_)
 {
-    static char *begin, *end_str, *end;
-    unsigned int i, j;
+    static char *begin, *endd_, *end2;
     int word_found = 0;
-
-    if (str == NULL)
+    
+    if(str == NULL)
     {
-        if (begin == NULL || end == NULL)
+        if(w_start == NULL || end2 == NULL)
             return NULL;
-        str = end + 1;
+        str = end2 + 1;
     }
     else
-        end_str = str + _strlen(str);
-
-    for (i = 0; str + i < end_str; i++)
+        endd_ = str + _strlen(str);
+        
+    for(unsigned int i = 0; str + i < endd_; i++)
     {
-        for (j = 0; delim != NULL && delim[j] != '\0'; j++)
+        for(unsigned int j = 0; delimit_ != NULL && delimit_[j] != '\0'; j++)
         {
-            if (str[i] == delim[j])
+            if(str[i] == delimit_[j])
             {
-                if (word_found == 1)
+                if(word_found == 1)
                 {
                     str[i] = '\0';
-                    end = str + i;
-                    return begin;
+                    end2 = str + i;
+                    return w_start;
                 }
                 break;
             }
         }
-        if (delim[j] == '\0' && word_found == 0)
+        if(delimit_[j] == '\0' && word_found == 0)
         {
             word_found = 1;
-            begin = str + i;
+            w_start = str + i;
         }
     }
-
-    end = NULL;
-    if (word_found == 1)
-        return begin;
-
+    
+    end2 = NULL;
+    if(word_found == 1)
+        return w_start;
+        
     return NULL;
 }
