e1f07175980d5e83befb80e12d19ee90158c69dc
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-05-24 21:32:54 +0200

    Update stok.c

--- stok.c
+++ stok.c
@@ -1,51 +1,90 @@
 #include "main.h"
-
 /**
- * _strtok - tokenizes a string
- * @str: string to tokenize
- * @delimit_: string that contains the delimiters
- * Return: pointer to position in string of a null-terminated word.
- */
-char *_strtok(char *str, const char *delimit_)
+
+  * _strtok - tokenizes a string
+
+  * @str: string to tokenize
+
+  * @delim: string that contains the delimiters
+
+  * Return: pointer to position in string of a null terminated word.
+
+  */
+
+char *_strtok(char *str, const char *delim)
+
 {
-    static char *w_start, *endd_, *end2;
+
+    static char *begin, *end_str, *end;
+    
     int word_found = 0;
-
-    if (str == NULL)
+    
+    
+    
+    if(str == NULL)
+    
     {
-        if (w_start == NULL || end2 == NULL)
-            return NULL;
-        str = end2 + 1;
+    
+        if(begin == NULL || end == NULL)
+        
+            return (NULL);
+            
+        str = end + 1;
+        
     }
+    
     else
-        endd_ = str + _strlen(str);
-
-    for (unsigned int i = 0; str + i < endd_; i++)
+    
+        end_str = str + _strlen(str);
+        
+    for(unsigned int i = 0; str + i < end_str; i++)
+    
     {
-        unsigned int j;
-        for (j = 0; delimit_ != NULL && delimit_[j] != '\0'; j++)
+    
+        for(unsigned int j = 0; delim != NULL && delim[j] != '\0'; j++)
+        
         {
-            if (str[i] == delimit_[j])
+        
+            if(str[i] == delim[j])
+            
             {
-                if (word_found == 1)
+            
+                if(word_found == 1)
+                
                 {
+                
                     str[i] = '\0';
-                    end2 = str + i;
-                    return w_start;
+                    
+                    end = str + i;
+                    
+                    return (begin);
+                    
                 }
+                
                 break;
+                
             }
+            
         }
-        if (delimit_[j] == '\0' && word_found == 0)
+        
+        if(delim[j] == '\0' && word_found == 0)
+        
         {
+        
             word_found = 1;
-            w_start = str + i;
+            
+            begin = str + i;
+            
         }
+        
     }
-
-    end2 = NULL;
-    if (word_found == 1)
-        return w_start;
-
-    return NULL;
+    
+    end = NULL;
+    
+    if(word_found == 1)
+    
+        return (begin);
+        
+    return (NULL);
+    
 }
