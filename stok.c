0ec69acefa1a324573103cfd46ee2bb2b43d8a49
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-05-24 21:29:00 +0200

    Update stok.c

--- stok.c
+++ stok.c
@@ -3,30 +3,31 @@
 /**
  * _strtok - tokenizes a string
  * @str: string to tokenize
- * @delimit_: string that contains the delimit_iters
+ * @delimit_: string that contains the delimiters
  * Return: pointer to position in string of a null-terminated word.
  */
 char *_strtok(char *str, const char *delimit_)
 {
     static char *w_start, *endd_, *end2;
     int word_found = 0;
-    
-    if(str == NULL)
+
+    if (str == NULL)
     {
-        if(w_start == NULL || end2 == NULL)
+        if (w_start == NULL || end2 == NULL)
             return NULL;
         str = end2 + 1;
     }
     else
         endd_ = str + _strlen(str);
-        
-    for(unsigned int i = 0; str + i < endd_; i++)
+
+    for (unsigned int i = 0; str + i < endd_; i++)
     {
-        for(unsigned int j = 0; delimit_ != NULL && delimit_[j] != '\0'; j++)
+        unsigned int j;
+        for (j = 0; delimit_ != NULL && delimit_[j] != '\0'; j++)
         {
-            if(str[i] == delimit_[j])
+            if (str[i] == delimit_[j])
             {
-                if(word_found == 1)
+                if (word_found == 1)
                 {
                     str[i] = '\0';
                     end2 = str + i;
@@ -35,16 +36,16 @@ char *_strtok(char *str, const char *delimit_)
                 break;
             }
         }
-        if(delimit_[j] == '\0' && word_found == 0)
+        if (delimit_[j] == '\0' && word_found == 0)
         {
             word_found = 1;
             w_start = str + i;
         }
     }
-    
+
     end2 = NULL;
-    if(word_found == 1)
+    if (word_found == 1)
         return w_start;
-        
+
     return NULL;
 }
