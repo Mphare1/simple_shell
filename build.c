d7b335d2c09c3cd2c81ef7d1afb16bbe01434f64
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-05-24 22:02:48 +0200

    Update build.c

--- build.c
+++ build.c
@@ -4,21 +4,21 @@
  * my_exit - simple impl of exit.
  * @ptrs: structure containing all malloced memory
  */
-void my_exit(shell_t *ptrs)
+void my_exit(shell_t *pts)
 {
-	unsigned int i;
-	char *exit_str;
-	exit_str = ptrs->input_token[1];
-	if (exit_str != NULL || ptrs == NULL)
-	{
-		errno = 0;
-		for (i = 0; exit_str[i] != '\0'; i++)
-			errno = errno * 10 + (exit_str[i] - '0');
-	}
-	free_shell_t(ptrs);
-	if (errno > 255)
-		errno %= 256;
-	exit(errno);
+    unsigned int x;
+    char *exit_str;
+    exit_str = pts->input_token[1];
+    if(exit_str != NULL || pts == NULL)
+    {
+        errno = 0;
+        for(x = 0; exit_str[x] != '\0'; x++)
+            errno = errno * 10 + (exit_str[x] - '0');
+    }
+    free_shell_t(ptrs);
+    if(errno > 255)
+        errno %= 256;
+    exit(errno);
 }
 /**
   * print_env - prints out the current environment
@@ -26,19 +26,19 @@ void my_exit(shell_t *ptrs)
   */
 void print_env(shell_t *ptrs)
 {
-	unsigned int i, k;
-	char newline = '\n';
-	(void)ptrs;
-	if (environ == NULL)
-		return;
-	for (i = 0; environ[i] != NULL; i++)
-	{
-		k = _strlen(environ[i]);
-		if (k != 0)
-		{
-			write(STDOUT_FILENO, environ[i], k);
-			write(STDOUT_FILENO, &newline, 1);
-		}
-	}
-	errno = 0;
+    unsigned int i, k;
+    char newline = '\n';
+    (void)ptrs;
+    if(environ == NULL)
+        return;
+    for(i = 0; environ[i] != NULL; i++)
+    {
+        k = _strlen(environ[i]);
+        if(k != 0)
+        {
+            write(STDOUT_FILENO, environ[i], k);
+            write(STDOUT_FILENO, &newline, 1);
+        }
+    }
+    errno = 0;
 }
