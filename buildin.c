406ea349c4a714c4159dcf45c5ebbe5efe7e7197
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-05-24 15:17:07 +0200

    Update buildin.c

--- buildin.c
+++ buildin.c
@@ -1,19 +1,14 @@
 #include "shell.h"
 
 /**
- * exit_function - Function pointer for the exit function.
- * @ptrs: Structure containing all malloced memory.
+ * my_exit - simple impl of exit.
+ * @ptrs: structure containing all malloced memory
  */
-typedef void (*exit_function)(shell_t *ptrs);
-
-/**
- * exit_default - Default implementation of the exit function.
- * @ptrs: Structure containing all malloced memory.
- */
-void exit_default(shell_t *ptrs)
+void my_exit(shell_t *ptrs)
 {
 	unsigned int i;
-	char *exit_str = ptrs->input_token[1];
+	char *exit_str;
+	exit_str = ptrs->input_token[1];
 	if (exit_str != NULL || ptrs == NULL)
 	{
 		errno = 0;
@@ -25,11 +20,10 @@ void exit_default(shell_t *ptrs)
 		errno %= 256;
 	exit(errno);
 }
-
 /**
- * print_env - Prints out the current environment.
- * @ptrs: Structure containing all malloced memory.
- */
+  * print_env - prints out the current environment
+  * @ptrs: structure containing all malloced memory
+  */
 void print_env(shell_t *ptrs)
 {
 	unsigned int i, k;
@@ -47,21 +41,4 @@ void print_env(shell_t *ptrs)
 		}
 	}
 	errno = 0;
-}
-
-/**
- * my_exit - Implementation of the exit function using function pointers and mal_mem.
- * @ptrs: Structure containing all malloced memory.
- */
-void my_exit(shell_t *ptrs)
-{
-	exit_function exit_fn = exit_default; // Default implementation
-	if (ptrs->input_token[1] != NULL)
-	{
-		if (strcmp(ptrs->input_token[1], "printenv") == 0)
-		{
-			exit_fn = print_env; // Use print_env function for "exit printenv"
-		}
-	}
-	exit_fn(ptrs); // Call the selected exit function
 }
